09bf530ba94231e026241156b19793308695a767
mdjhoel <mdjhoel@gmail.com>
2020-06-15 14:54:41 -0400

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,22 @@
+#print("hello world")
+
+#name = input("best country in the world: ")
+#print(name)
+
+'''
+name = name.lower()
+if (name == "canada"):
+    print("--------")
+    print(name + " is the best!")
+elif (name == "norway"):
+    print(name + " is the 2nd best!")
+else:
+    print("booo wrong answer")
+'''
+
+fruits = ['apple,grannysmith,gala','orange,mandarin,bloodorange','banana,big,small','peach,sweet,not sweet']
+for fruit in fruits:
+    templist = fruit.split(",")
+    print(fruit)
+    print(templist)
+
